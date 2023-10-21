4ee22c3683e362829c1df4db700d834355fa7bbe
Francisco Zuidwijk <electrocisko@gmail.com>
2023-10-21 11:27:58 -0300

    google passport basico funcionando

--- config/configEnv.js
+++ config/configEnv.js
@@ -18,4 +18,8 @@ export default {
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
   },
+  google: {
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET:process.env.GOOGLE_CLIENT_SECRET
+  }
 };
--- config/configPassport.js
+++ config/configPassport.js
@@ -2,76 +2,105 @@ import passport from "passport";
 import local from "passport-local";
 import { User } from "../models/userModel.js";
 import { createHash, isValidPassword } from "../helpers/cryptPassword.js";
-import GitHubStrategy from 'passport-github2';
+import GitHubStrategy from "passport-github2";
+import passportGoogleAuth2 from "passport-google-oauth2";
 import configEnv from "./configEnv.js";
 
 const LocalStrategy = local.Strategy;
+const GoogleStrategy = passportGoogleAuth2.Strategy;
 
 const initPassportLocal = () => {
-try {
+  try {
     passport.use(
-        "register",
-        new LocalStrategy(
-          { passReqToCallback: true },
-          async (req, username, password, done) => {
-            //Faltan validaciones de que lleguen los datos
-            const userData = {
-              username,
-              password,
-            };
-            const exist = await User.findOne({ username });
-            if (exist)
-              return done(null, false, { message: "usuario ya registrado" });
-            let newUser = new User(userData);
-            newUser.password = await createHash(req.body.password);
-            const user = await newUser.save();
-            if (!user)
-              return done(null, false, { message: "Error al crear usuario" });
-            return done(null, user);
-          }
-        )
-      );
-} catch (error) {
-    return done(error, false)
-}
+      "register",
+      new LocalStrategy(
+        { passReqToCallback: true },
+        async (req, username, password, done) => {
+          //Faltan validaciones de que lleguen los datos
+          const userData = {
+            username,
+            password,
+          };
+          const exist = await User.findOne({ username });
+          if (exist)
+            return done(null, false, { message: "usuario ya registrado" });
+          let newUser = new User(userData);
+          newUser.password = await createHash(req.body.password);
+          const user = await newUser.save();
+          if (!user)
+            return done(null, false, { message: "Error al crear usuario" });
+          return done(null, user);
+        }
+      )
+    );
+  } catch (error) {
+    return done(error, false);
+  }
 
   passport.use(
     "login",
-    new LocalStrategy(
-    async (username, password, done) => {
+    new LocalStrategy(async (username, password, done) => {
       const userDB = await User.findOne({ username });
-      if (!userDB) return done(null,false, {message: "No existe usuario"});
+      if (!userDB) return done(null, false, { message: "No existe usuario" });
       const valid = await isValidPassword(userDB.password, password);
       if (valid) {
-        return done(null, userDB)
+        return done(null, userDB);
       }
-      return done(null,false, {message: "error en validacion de usuario"})
+      return done(null, false, { message: "error en validacion de usuario" });
     })
   );
 
-  passport.use(new GitHubStrategy({
-    clientID: configEnv.github.GITHUB_CLIENT_ID,
-    clientSecret: configEnv.github.GITHUB_CLIENT_SECRET,
-    callbackURL: "http://localhost:3030/githubcallback"
-  },
-   async function(accessToken, refreshToken, profile, done) {
-    try {
-    const user = await  User.create({ username: profile._json.login })
-     return done(null,user)
-    } catch (error) {
-      return done(err, false);
-    }
-  }
-));
-  
-  passport.serializeUser((user, done) => {
-    done(null, user._id);
-  }); 
+  passport.use(
+    new GitHubStrategy(
+      {
+        clientID: configEnv.github.GITHUB_CLIENT_ID,
+        clientSecret: configEnv.github.GITHUB_CLIENT_SECRET,
+        callbackURL: "http://localhost:3030/githubcallback",
+      },
+      async function (accessToken, refreshToken, profile, done) {
+        try {
+          const user = await User.create({ username: profile._json.login });
+          return done(null, user);
+        } catch (error) {
+          return done(err, false);
+        }
+      }
+    )
+  );
+  /////////////////GOOGLE /////////////////////////////////
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: configEnv.google.GOOGLE_CLIENT_ID,
+        clientSecret: configEnv.google.GOOGLE_CLIENT_SECRET,
+        callbackURL: "http://localhost:3030/googlecallback",
+        passReqToCallback: true,
+      },
+      function (request, accessToken, refreshToken, profile, done) {
+        // User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        //   return done(err, user);
+        // });
+        return done(null,profile);
+      }
+    )
+  );
 
-  passport.deserializeUser(async (id, done) => {
-    let result = await User.findById(id);
-    return done(null, result);
-  });
+  // passport.serializeUser((user, done) => {
+  //   done(null, user._id);
+  // });
+
+  // passport.deserializeUser(async (id, done) => {
+  //   let result = await User.findById(id);
+  //   return done(null, result);
+  // });
 };
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
 
 export default initPassportLocal;
